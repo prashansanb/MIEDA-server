4563e35ca7fd2d68b92fb90388bbd1e65b0e9ed6
frextrite <29180487+frextrite@users.noreply.github.com>
2018-03-24 23:20:40 +0530

    POSTed base64 string is converted to image and stored

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, request
+import base64
+from flask import Flask, request, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -6,9 +8,26 @@ app = Flask(__name__)
 def getJSONData():
     data_json = request.get_json() # get JSON object
     ''' @TODO:
-    1. get base64 String
-    2. parse base64 string and convert to image
+    1. get base64 String - Done
+    2. parse base64 string and convert to image - Done
     3. implement sentiment analysis
     4. return results
     '''
-    return "Call Successful"
+    base64String = data_json['imgBase64']
+    filename = get_file_name()
+
+    with open(filename, 'wb') as image:
+        image.write(base64.b64decode(base64String))
+
+    analysis = emotion_api(filename)
+
+    return jsonify(analysis)
+
+
+def get_file_name():
+    return datetime.now().strftime('IMG_%Y%m%d_%H%M%S.jpg')
+
+def emotion_api(filename):
+    '''@TODO: Implement analysis'''
+    analyzed_data = {"null": "null"}
+    return analyzed_data
